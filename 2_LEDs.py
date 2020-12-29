899f96ee20f1d05072b84fc92334f9a7a3830ae7
Eric Burns <ericburnsonline@users.noreply.github.com>
2020-12-29 10:48:54 -0800

    Create 2_LEDs.py
    Initial base

---
+++ 2_LEDs.py
@@ -0,0 +1,18 @@
+from gpiozero import LED
+import RPi.GPIO as GPIO
+from time import sleep
+
+led_pins = [13, 19, 20, 21, 26]
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+for led in led_pins:
+	GPIO.setup(led, GPIO.OUT)
+
+while True:
+	for led in led_pins:
+		GPIO.output(led, True)
+		sleep(.2)
+		GPIO.output(led, False)
+	sleep(2)
